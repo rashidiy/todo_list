25ca0052e7646a2d0b4b4b6cefc1a265638106d8
rashideveloper <bubbless7456@gmail.com>
2022-12-24 12:00:18 +0500

    TodoView

--- apps/views/todo.py
+++ apps/views/todo.py
@@ -1,6 +1,10 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import ListView
+from django.db import transaction
+from django.urls import reverse_lazy
+from django.views import View
+from django.views.generic import ListView, CreateView
 
+from apps.forms import PositionForm
 from apps.models import Task
 
 
@@ -9,4 +13,27 @@ class TodoListView(LoginRequiredMixin, ListView):
     context_object_name = 'tasks'
 
     def get_queryset(self):
-        return Task.objects.filter(user_id=self.request.user.pk)
+        return Task.objects.filter(user=self.request.user)
+
+
+class TodoReorder(View):
+    def post(self, request):
+        form = PositionForm(request.POST)
+
+        if form.is_valid():
+            position_list = form.cleaned_data["position"].split(',')
+
+            with transaction.atomic():
+                self.request.user.set_task_order(position_list)
+
+        return reverse_lazy('todo')
+
+
+class TodoCreateView(LoginRequiredMixin, CreateView):
+    model = Task
+    fields = ['title', 'description', 'status']
+    success_url = reverse_lazy('tasks')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(TodoCreateView, self).form_valid(form)
