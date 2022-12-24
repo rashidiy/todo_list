85a7a7ed5eba7cb737a2f688fdd6f4ae751dd17b
rashideveloper <bubbless7456@gmail.com>
2022-12-24 10:56:00 +0500

    urls

--- apps/urls.py
+++ apps/urls.py
@@ -1,7 +1,14 @@
-from django.urls import path
+from django.contrib.auth.views import LogoutView
+from django.urls import path, reverse_lazy
 
-from .views import CreateUserView
+from .views import AuthRegisterView
+from .views.auth import AuthLoginView
+from .views.todo import TodoListView
 
 urlpatterns = [
-    path('accounts/register', CreateUserView.as_view(), name='register'),
+    path('accounts/register', AuthRegisterView.as_view(), name='register'),
+    path('accounts/login', AuthLoginView.as_view(), name='login'),
+    path('accounts/logout', LogoutView.as_view(next_page=reverse_lazy('login')), name='logout'),
+
+    path('todo', TodoListView.as_view(), name='todo'),
 ]
--- root/urls.py
+++ root/urls.py
@@ -20,4 +20,3 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('apps.urls')),
 ]
-
