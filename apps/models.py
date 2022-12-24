3978bb2a5cf1af4a367a919eb9e0d7ecebd603a3
rashideveloper <bubbless7456@gmail.com>
2022-12-24 11:04:32 +0500

    task model

--- apps/models.py
+++ apps/models.py
@@ -1,9 +1,11 @@
-from django.db.models import Model, CharField, BooleanField
+from django.contrib.auth.models import User
+from django.db.models import Model, CharField, BooleanField, ForeignKey, CASCADE
 
 
 # Create your models here.
 
 class Task(Model):
+    user = ForeignKey(User, CASCADE)
     title = CharField(max_length=255)
     description = CharField(max_length=1024, blank=True, null=True)
     status = BooleanField(default=False)
