dd79cb2de3c0ca4afebf9cea1a61c431474dc0cc
rashideveloper <bubbless7456@gmail.com>
2022-12-24 11:25:22 +0500

    TodoList

--- apps/views/todo.py
+++ apps/views/todo.py
@@ -1,5 +1,12 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView
 
+from apps.models import Task
 
-class TodoListView(ListView):
+
+class TodoListView(LoginRequiredMixin, ListView):
     template_name = 'apps/task_list.html'
+    context_object_name = 'tasks'
+
+    def get_queryset(self):
+        return Task.objects.filter(user_id=self.request.user.pk)
