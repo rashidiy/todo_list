8e4cec4061dc9b93230e0d6f89391a0b84e97406
rashideveloper <bubbless7456@gmail.com>
2022-12-24 10:57:03 +0500

    auth view

--- apps/views/auth.py
+++ apps/views/auth.py
@@ -1,5 +1,8 @@
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.hashers import make_password
+from django.contrib.auth.views import LoginView
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
 from apps.forms import UserRegisterForm
@@ -8,11 +11,17 @@ from apps.forms import UserRegisterForm
 # Create your views here.
 
 
-class CreateUserView(CreateView):
+class AuthRegisterView(CreateView):
     form_class = UserRegisterForm
     template_name = 'apps/register.html'
+    success_url = reverse_lazy('register')
 
     def form_valid(self, form):
-        form.instance.password = make_password(form.instance.password1)
+        form.instance.password = make_password(form.cleaned_data.get('password'))
         return super().form_valid(form)
 
+
+class AuthLoginView(LoginView):
+    template_name = 'apps/login.html'
+    next_page = reverse_lazy('login')
+    form_class = AuthenticationForm
