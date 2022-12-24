d0917855238ba4cffb824ca248b7b30293978327
rashideveloper <bubbless7456@gmail.com>
2022-12-24 10:56:49 +0500

    models

--- apps/models.py
+++ apps/models.py
@@ -1,3 +1,9 @@
-from django.db import models
+from django.db.models import Model, CharField, BooleanField
+
 
 # Create your models here.
+
+class Task(Model):
+    title = CharField(max_length=255)
+    description = CharField(max_length=1024, blank=True, null=True)
+    status = BooleanField(default=False)
