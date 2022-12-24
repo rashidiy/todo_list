27d3e101e1962e6336ab7432d81398884708df47
rashideveloper <bubbless7456@gmail.com>
2022-12-24 18:33:15 +0500

    urls

--- apps/urls.py
+++ apps/urls.py
@@ -3,14 +3,16 @@ from django.urls import path, reverse_lazy
 
 from .views import AuthRegisterView
 from .views.auth import AuthLoginView
-from .views.todo import TodoListView, TodoReorder, TodoCreateView
+from .views.todo import TodoListView, TodoReorder, TodoCreateView, TodoUpdateView, TodoDeleteView
 
 urlpatterns = [
     path('accounts/register', AuthRegisterView.as_view(), name='register'),
     path('accounts/login', AuthLoginView.as_view(), name='login'),
     path('accounts/logout', LogoutView.as_view(next_page=reverse_lazy('login')), name='logout'),
 
-    path('todo', TodoListView.as_view(), name='todo'),
+    path('', TodoListView.as_view(), name='todo'),
     path('todo-reorder', TodoReorder.as_view(), name='todo_reorder'),
     path('todo-create', TodoCreateView.as_view(), name='todo_create'),
+    path('todo_update/<int:pk>', TodoUpdateView.as_view(), name='todo_update'),
+    path('todo_delete/<int:pk>', TodoDeleteView.as_view(), name='todo_delete'),
 ]
